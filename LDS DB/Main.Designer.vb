1fdb41e67370191a22b0b85abc9973fbf923be2e
Cristian Suárez <gecocr@hotmail.com>
2013-08-22 11:01:51 -0600

    Initial_Commit

---
+++ LDS DB/Main.Designer.vb
@@ -0,0 +1,183 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Me.stpMainMenu = New System.Windows.Forms.MenuStrip
+        Me.mnuSystem = New System.Windows.Forms.ToolStripMenuItem
+        Me.mnuExit = New System.Windows.Forms.ToolStripMenuItem
+        Me.StatusStrip = New System.Windows.Forms.StatusStrip
+        Me.lblStatus = New System.Windows.Forms.ToolStripStatusLabel
+        Me.prgBar = New System.Windows.Forms.ToolStripProgressBar
+        Me.ToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.tlsTools = New System.Windows.Forms.ToolStrip
+        Me.cmdBIReport = New System.Windows.Forms.ToolStripButton
+        Me.cmdMyPReport = New System.Windows.Forms.ToolStripButton
+        Me.cmdAccentureReport = New System.Windows.Forms.ToolStripButton
+        Me.ofdFile = New System.Windows.Forms.OpenFileDialog
+        Me.DS = New System.Data.DataSet
+        Me.BGW = New System.ComponentModel.BackgroundWorker
+        Me.stpMainMenu.SuspendLayout()
+        Me.StatusStrip.SuspendLayout()
+        Me.tlsTools.SuspendLayout()
+        CType(Me.DS, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'stpMainMenu
+        '
+        Me.stpMainMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuSystem})
+        Me.stpMainMenu.Location = New System.Drawing.Point(0, 0)
+        Me.stpMainMenu.Name = "stpMainMenu"
+        Me.stpMainMenu.Size = New System.Drawing.Size(343, 24)
+        Me.stpMainMenu.TabIndex = 5
+        Me.stpMainMenu.Text = "MenuStrip"
+        '
+        'mnuSystem
+        '
+        Me.mnuSystem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.mnuExit})
+        Me.mnuSystem.ImageTransparentColor = System.Drawing.SystemColors.ActiveBorder
+        Me.mnuSystem.Name = "mnuSystem"
+        Me.mnuSystem.Size = New System.Drawing.Size(57, 20)
+        Me.mnuSystem.Text = "&System"
+        '
+        'mnuExit
+        '
+        Me.mnuExit.Name = "mnuExit"
+        Me.mnuExit.Size = New System.Drawing.Size(92, 22)
+        Me.mnuExit.Text = "E&xit"
+        '
+        'StatusStrip
+        '
+        Me.StatusStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.lblStatus, Me.prgBar})
+        Me.StatusStrip.Location = New System.Drawing.Point(0, 238)
+        Me.StatusStrip.Name = "StatusStrip"
+        Me.StatusStrip.Size = New System.Drawing.Size(343, 22)
+        Me.StatusStrip.TabIndex = 7
+        Me.StatusStrip.Text = "StatusStrip"
+        '
+        'lblStatus
+        '
+        Me.lblStatus.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(507, 17)
+        Me.lblStatus.Spring = True
+        Me.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'prgBar
+        '
+        Me.prgBar.Name = "prgBar"
+        Me.prgBar.Size = New System.Drawing.Size(100, 16)
+        '
+        'tlsTools
+        '
+        Me.tlsTools.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.tlsTools.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.cmdMyPReport, Me.cmdBIReport, Me.cmdAccentureReport})
+        Me.tlsTools.Location = New System.Drawing.Point(0, 24)
+        Me.tlsTools.Name = "tlsTools"
+        Me.tlsTools.Size = New System.Drawing.Size(343, 31)
+        Me.tlsTools.TabIndex = 9
+        Me.tlsTools.Text = "ToolStrip1"
+        '
+        'cmdBIReport
+        '
+        Me.cmdBIReport.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.cmdBIReport.Image = Global.LDS_DB.My.Resources.Resources.folder_developer2
+        Me.cmdBIReport.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.cmdBIReport.Name = "cmdBIReport"
+        Me.cmdBIReport.Size = New System.Drawing.Size(28, 28)
+        Me.cmdBIReport.Text = "ToolStripButton1"
+        Me.cmdBIReport.ToolTipText = "Upload BI Report"
+        '
+        'cmdMyPReport
+        '
+        Me.cmdMyPReport.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.cmdMyPReport.Image = Global.LDS_DB.My.Resources.Resources.folder_green
+        Me.cmdMyPReport.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.cmdMyPReport.Name = "cmdMyPReport"
+        Me.cmdMyPReport.Size = New System.Drawing.Size(28, 28)
+        Me.cmdMyPReport.Text = "ToolStripButton2"
+        Me.cmdMyPReport.ToolTipText = "Upload MyPurch Report"
+        '
+        'cmdAccentureReport
+        '
+        Me.cmdAccentureReport.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.cmdAccentureReport.Image = Global.LDS_DB.My.Resources.Resources.folder_red
+        Me.cmdAccentureReport.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.cmdAccentureReport.Name = "cmdAccentureReport"
+        Me.cmdAccentureReport.Size = New System.Drawing.Size(28, 28)
+        Me.cmdAccentureReport.Text = "ToolStripButton3"
+        Me.cmdAccentureReport.ToolTipText = "Upload Accenture Report"
+        '
+        'ofdFile
+        '
+        Me.ofdFile.FileName = "OpenFileDialog1"
+        '
+        'DS
+        '
+        Me.DS.DataSetName = "NewDataSet"
+        '
+        'BGW
+        '
+        Me.BGW.WorkerReportsProgress = True
+        Me.BGW.WorkerSupportsCancellation = True
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(343, 260)
+        Me.Controls.Add(Me.tlsTools)
+        Me.Controls.Add(Me.stpMainMenu)
+        Me.Controls.Add(Me.StatusStrip)
+        Me.IsMdiContainer = True
+        Me.MainMenuStrip = Me.stpMainMenu
+        Me.Name = "Main"
+        Me.Text = "Main"
+        Me.stpMainMenu.ResumeLayout(False)
+        Me.stpMainMenu.PerformLayout()
+        Me.StatusStrip.ResumeLayout(False)
+        Me.StatusStrip.PerformLayout()
+        Me.tlsTools.ResumeLayout(False)
+        Me.tlsTools.PerformLayout()
+        CType(Me.DS, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ToolTip As System.Windows.Forms.ToolTip
+    Friend WithEvents lblStatus As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents StatusStrip As System.Windows.Forms.StatusStrip
+    Friend WithEvents mnuExit As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents mnuSystem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents stpMainMenu As System.Windows.Forms.MenuStrip
+    Friend WithEvents tlsTools As System.Windows.Forms.ToolStrip
+    Friend WithEvents cmdBIReport As System.Windows.Forms.ToolStripButton
+    Friend WithEvents cmdMyPReport As System.Windows.Forms.ToolStripButton
+    Friend WithEvents cmdAccentureReport As System.Windows.Forms.ToolStripButton
+    Friend WithEvents ofdFile As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents DS As System.Data.DataSet
+    Friend WithEvents BGW As System.ComponentModel.BackgroundWorker
+    Friend WithEvents prgBar As System.Windows.Forms.ToolStripProgressBar
+
+End Class
---
+++ LDS DB/Main.vb
@@ -0,0 +1,160 @@
+﻿Imports System.Windows.Forms
+
+Public Class Main
+    Public FileName As String
+
+#Region "Form methods"
+#Region "Menu Options"
+    Private Sub ExitToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles mnuExit.Click
+        End
+    End Sub
+#End Region
+#Region "Toolbar methods"
+    Private Sub cmdBIReport_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdBIReport.Click
+        FileName = ""
+        lblStatus.Text = "Getting file..."
+        prgBar.Style = ProgressBarStyle.Marquee
+
+        ofdFile.InitialDirectory = My.Computer.FileSystem.SpecialDirectories.Desktop
+        ofdFile.Filter = "Excel 2010(*.xlsx)|*.xlsx"
+        ofdFile.Title = "Searching: BI Report"
+
+        If ofdFile.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            FileName = ofdFile.FileName
+            If FileName.Length > 0 Then
+                lblStatus.Text = "Reading file..."
+                BGW.RunWorkerAsync("BI Report")
+            End If
+        End If
+    End Sub
+    Private Sub cmdMyPReport_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdMyPReport.Click
+        FileName = ""
+        lblStatus.Text = "Getting file..."
+        prgBar.Style = ProgressBarStyle.Marquee
+
+        ofdFile.InitialDirectory = My.Computer.FileSystem.SpecialDirectories.Desktop
+        ofdFile.Filter = "Excel 2010(*.xlsx)|*.xlsx"
+        ofdFile.Title = "Searching: My Purch. Report"
+
+
+        If ofdFile.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            FileName = ofdFile.FileName
+            If FileName.Length > 0 Then
+                lblStatus.Text = "Reading file..."
+                BGW.RunWorkerAsync("My Purch")
+            End If
+        End If
+    End Sub
+    Private Sub cmdAccentureReport_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdAccentureReport.Click
+        FileName = ""
+        lblStatus.Text = "Getting file..."
+        prgBar.Style = ProgressBarStyle.Marquee
+        ofdFile.InitialDirectory = My.Computer.FileSystem.SpecialDirectories.Desktop
+        ofdFile.Filter = "Excel 2010(*.xlsx)|*.xlsx"
+        ofdFile.Title = "Searching: Accenture Report"
+        If ofdFile.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            FileName = ofdFile.FileName
+            If FileName.Length > 0 Then
+                lblStatus.Text = "Reading file..."
+                BGW.RunWorkerAsync("Accenture")
+            End If
+        End If
+    End Sub
+#End Region
+#Region "Others"
+    Private Sub BGW_DoWork(ByVal sender As System.Object, ByVal e As System.ComponentModel.DoWorkEventArgs) Handles BGW.DoWork
+        Dim XLSheet As String = ""
+        Dim Table_Name As String = ""
+        Dim SQLTable As String = ""
+        Dim StoreProc As String = ""
+
+        'Select the report type to be uploaded
+        Select Case e.Argument
+            Case "My Purch"
+                XLSheet = "MyP SC report.xls"
+                Table_Name = "MyPData"
+                SQLTable = "TMP_My Purchases Report"
+                StoreProc = "stp_Update_MyPurch_Report"
+            Case "BI Report"
+                XLSheet = "SAPBW_DOWNLOAD"
+                Table_Name = "BIData"
+                SQLTable = "TMP_BI Report"
+                StoreProc = "stp_Update_BI_Report"
+            Case "Accenture"
+                XLSheet = "Tracking Sheet"
+                Table_Name = "AccTable"
+                SQLTable = "TMP_Accenture"
+                StoreProc = "stp_Update_Accenture_Report"
+        End Select
+
+        'Las instrucciones del dataset estan comentadas para futuro de informnacion local,
+        'se utilizará una vista ya que esta tool de un solo usuario y su BK.
+
+        Dim XLData As New DataTable
+        Dim xl As New Objects.MSExcel
+
+        Try
+            'If DS.Tables.IndexOf("BIData") > -1 Then
+            '    DS.Tables.Remove("BIData")
+            'End If
+
+            XLData = xl.Read_XLSX(FileName, XLSheet, True) ' Read the Excel file
+            XLData.TableName = Table_Name ' Assign the table name
+
+            If XLData.TableName = "AccTable" Then
+                'Accenture report has columns that need to be deleted
+                If XLData.Columns.Count > 62 Then 'If the report has more then 62 columns
+                    Do
+                        XLData.Columns.RemoveAt(XLData.Columns.Count - 1) 'Remove the column
+                    Loop Until XLData.Columns.Count = 62
+                    XLData.AcceptChanges()
+                End If
+
+                'Some rows has blank @ Primary Key SC# or SC Line# 
+                Dim RN As Integer = 0
+                For Each R As DataRow In XLData.Rows
+                    RN += 1
+                    If (R("SC_#").ToString.Trim.Length = 0) Or (R("SC_Line").ToString.Trim.Length = 0) Then
+                        R("SC_#") = -1 'This row will be deleted @ Stored procedure: stp_Update_Accenture_Report
+                        R("SC_Line") = RN 'This row will be deleted @ Stored procedure: stp_Update_Accenture_Report
+                    End If
+                Next
+            End If
+
+            BGW.ReportProgress(0, "Uploading to server...")
+            If XLData.Rows.Count > 0 Then
+                'DS.Tables.Add(BIData.Copy())
+                Dim cn As New Objects.Connection
+                cn.Execute("DELETE FROM [" & SQLTable & "]") 'Cleaning temporary table
+                If cn.AppendTableToSqlServer("[" & SQLTable & "]", XLData) Then 'Append report to temporary table
+                    Dim SP As New Objects.Stored_Procedure
+                    BGW.ReportProgress(0, "Uploading new records...")
+                    SP.Stored_Procedure_Name = StoreProc
+                    If Not SP.Execute() Then 'Excecute stored procedure
+                        MsgBox("Unable to update history.", MsgBoxStyle.Exclamation)
+                    End If
+                Else
+                    MsgBox("Unable to load file to server.")
+                End If
+            End If
+
+        Catch ex As Exception
+            'To do control if any exception raise.
+            MsgBox("Unknown error: " & ex.Message & Chr(13) & Chr(13) & "Unable to finish the process.", MsgBoxStyle.Critical, "Unknown error")
+
+        End Try
+    End Sub
+
+    Private Sub BGW_ProgressChanged(ByVal sender As Object, ByVal e As System.ComponentModel.ProgressChangedEventArgs) Handles BGW.ProgressChanged
+        lblStatus.Text = e.UserState
+    End Sub
+
+    Private Sub BGW_RunWorkerCompleted(ByVal sender As Object, ByVal e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles BGW.RunWorkerCompleted
+        lblStatus.Text = "Process finished."
+        prgBar.Style = ProgressBarStyle.Blocks
+        MsgBox("Process finished.", MsgBoxStyle.Information)
+    End Sub
+#End Region
+
+#End Region
+End Class
